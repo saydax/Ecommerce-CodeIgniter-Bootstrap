94488cd0621959eaedf5a6957e56889441a865c4
saydax <saydax@tnctr.com>
2018-10-23 17:27:59 +0300

    add gitignore

---
+++ application/logs/log-2018-10-23.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-10-23 16:24:44 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\Ecommerce-CodeIgniter-Bootstrap\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2018-10-23 16:24:44 --> Unable to connect to the database
